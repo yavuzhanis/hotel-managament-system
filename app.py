6d7f2233ca46c18144f85bb27b87f90c6fa1c688
Yavuzhan <yavuzhan.is@kun.edu.tr>
2024-09-04 15:44:26 +0300

    fınısh-homepage

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ class HotelManagamentSystem:
         self.root.title("Hotel Managament System")
         self.root.geometry("1700x900+0+0")
         # ! ÜST IMG
-        img1 = Image.open(r"./images/img-back.jpg")
+        img1 = Image.open(r"./images/hotel.png")
         img1 = img1.resize((1700, 180), Image.LANCZOS)
         self.photoImg1 = ImageTk.PhotoImage(img1)
         lblImg = Label(self.root, image=self.photoImg1, bd=4, relief=RIDGE)
@@ -32,7 +32,7 @@ class HotelManagamentSystem:
         lbl_title.place(x=0, y=180, width=1750, height=50)
         #! main frame
         main_frame = Frame(self.root, bd=4, relief=RIDGE)
-        main_frame.place(x=0, y=220, width=1750, height=620)
+        main_frame.place(x=0, y=220, width=1750, height=610)
         #! MENU
         lbl_menu = Label(
             main_frame,
@@ -96,6 +96,7 @@ class HotelManagamentSystem:
             cursor="hand1",
         )
         report_btn.grid(row=3, column=0, pady=1)
+   
         logout_btn = Button(
             btn_frame,
             text="LOGOUT",
@@ -107,6 +108,42 @@ class HotelManagamentSystem:
             cursor="hand1",
         )
         logout_btn.grid(row=4, column=0, pady=1)
+
+        #! right side img
+
+        img3 = Image.open(r"./images/reception.jpg")
+        img3 = img3.resize((1700, 590), Image.LANCZOS)
+        self.photoImg3 = ImageTk.PhotoImage(img3)
+        lblImg1 = Label(main_frame, image=self.photoImg3, bd=4, relief=RIDGE)
+        lblImg1.place(x=225, y=0, width=1700, height=600)
+
+        #! down image
+
+        img4 = Image.open(r"./images/eat-hotel.png")
+        img4 = img4.resize((230, 210), Image.LANCZOS)
+        self.photoImg4 = ImageTk.PhotoImage(img4)
+        lblImg2 = Label(main_frame, image=self.photoImg4, bd=4, relief=RIDGE)
+        lblImg2.place(x=0, y=225, width=230, height=210)
+
+
+
+        img5 = Image.open(r"./images/hotel-small.jpeg")
+        img5 = img5.resize((230, 190), Image.LANCZOS)
+        self.photoImg5 = ImageTk.PhotoImage(img5)
+        lblImg3 = Label(main_frame, image=self.photoImg5, bd=4, relief=RIDGE)
+        lblImg3.place(x=0, y=420, width=230, height=190)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
