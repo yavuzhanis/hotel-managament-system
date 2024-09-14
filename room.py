5c14eaa2903c99d4901140b1be9c99a71f61d44b
Yavuzhan <yavuzhan.is@kun.edu.tr>
2024-09-14 21:13:30 +0300

    update-func

--- room.py
+++ room.py
@@ -63,6 +63,157 @@ class RoomBooking:
             state="readonly",
         )
         enty_contact.grid(row=0, column=1)
+        #! check IN date
+        check_in_date = Label(
+            LabelFrameleft,
+            font=("times new roman", 13, "bold"),
+            text="Check In DATE:",
+            padx=2,
+            pady=6,
+        )
+        check_in_date.grid(row=1, column=0, sticky=W)
+        text_check_in_date = ttk.Entry(
+            LabelFrameleft,
+            width=26,
+            font=("times new roman", 13, "bold"),
+        )
+        text_check_in_date.grid(row=1, column=1)
+        #! check OUT date
+        check_out_date = Label(
+            LabelFrameleft,
+            font=("times new roman", 13, "bold"),
+            text="CheckOUT DATE:",
+            padx=2,
+            pady=6,
+        )
+        check_out_date.grid(row=2, column=0, sticky=W)
+        text_check_out_date = ttk.Entry(
+            LabelFrameleft,
+            width=26,
+            font=("times new roman", 13, "bold"),
+        )
+        text_check_out_date.grid(row=2, column=1)
+
+        # ? ROOM TYPE
+        label_Room_Type = Label(
+            LabelFrameleft,
+            font=("times new roman", 13, "bold"),
+            text="Room Type:",
+            padx=2,
+            pady=6,
+        )
+        label_Room_Type.grid(row=3, column=0, sticky=W)
+
+        combo_roomType = ttk.Combobox(
+            LabelFrameleft,
+            font=("arial", 12, "bold"),
+            width=27,
+            state="readonly",
+        )
+        combo_roomType["value"] = ("Single", "Double", "Luxury")
+        combo_roomType.current(0)
+        combo_roomType.grid(row=3, column=1)
+
+        # TODO avaliable ROOM
+        lblRoomAvaliable = Label(
+            LabelFrameleft,
+            font=("times new roman", 13, "bold"),
+            text="Avaliable Room:",
+            padx=2,
+            pady=6,
+        )
+        lblRoomAvaliable.grid(row=4, column=0, sticky=W)
+
+        lblRoomAvaliable_text = ttk.Entry(
+            LabelFrameleft,
+            width=26,
+            font=("times new roman", 13, "bold"),
+        )
+        lblRoomAvaliable_text.grid(row=4, column=1)
+        #! meals
+        lblMeal = Label(
+            LabelFrameleft,
+            font=("times new roman", 13, "bold"),
+            text=" Meals:",
+            padx=2,
+            pady=6,
+        )
+        lblMeal.grid(row=5, column=0, sticky=W)
+
+        lblMeal_text = ttk.Entry(
+            LabelFrameleft,
+            width=26,
+            font=("times new roman", 13, "bold"),
+        )
+        lblMeal_text.grid(row=5, column=1)
+
+        # ? NO OF DAYS
+        lblNo_OfDays = Label(
+            LabelFrameleft,
+            font=("times new roman", 13, "bold"),
+            text=" No Of Days:",
+            padx=2,
+            pady=6,
+        )
+        lblNo_OfDays.grid(row=6, column=0, sticky=W)
+
+        lblMeal_text = ttk.Entry(
+            LabelFrameleft,
+            width=26,
+            font=("times new roman", 13, "bold"),
+        )
+        lblMeal_text.grid(row=6, column=1)
+
+        # ? paid tax
+        lblpaid_tax = Label(
+            LabelFrameleft,
+            font=("times new roman", 13, "bold"),
+            text=" Paid Tax:",
+            padx=2,
+            pady=6,
+        )
+        lblpaid_tax.grid(row=7, column=0, sticky=W)
+
+        lblpaid_tax_text = ttk.Entry(
+            LabelFrameleft,
+            width=26,
+            font=("times new roman", 13, "bold"),
+        )
+        lblpaid_tax_text.grid(row=7, column=1)
+
+        # ? Sub total
+        lblSubTotal = Label(
+            LabelFrameleft,
+            font=("times new roman", 13, "bold"),
+            text="Sub Total:",
+            padx=2,
+            pady=6,
+        )
+        lblSubTotal.grid(row=8, column=0, sticky=W)
+
+        lblSubTotal_text = ttk.Entry(
+            LabelFrameleft,
+            width=26,
+            font=("times new roman", 13, "bold"),
+        )
+        lblSubTotal_text.grid(row=8, column=1)
+
+        # ? total cost
+        lblIdNumber = Label(
+            LabelFrameleft,
+            font=("times new roman", 13, "bold"),
+            text=" Total Cost:",
+            padx=2,
+            pady=6,
+        )
+        lblIdNumber.grid(row=9, column=0, sticky=W)
+
+        lblIdNumber_text = ttk.Entry(
+            LabelFrameleft,
+            width=26,
+            font=("times new roman", 13, "bold"),
+        )
+        lblIdNumber_text.grid(row=9, column=1)
 
 
 if __name__ == "__main__":
